8bb42a0226a0696d5ec74805b9492826846feac0
Joe S <17176434+joeselway@users.noreply.github.com>
2019-05-29 00:46:47 +0930

    Update saedu-ca-trust-pkg-postflight.sh

--- schools.sa.edu.au/saedu-ca-trust-pkg-postflight.sh
+++ schools.sa.edu.au/saedu-ca-trust-pkg-postflight.sh
@@ -4,11 +4,9 @@ echo 'Installing SAEDU Issuing CA'
 
 mkdir /tmp/saedu-ca
 
-cd /tmp/saedu-ca
+curl -k -o "/tmp/saedu-ca/saedu-int-ca.cer" "http://ca.schools.sa.edu.au/SAEDUIssuingCA_SHA2.cer"
 
-curl -k -o "saedu-int-ca.cer" "http://ca.schools.sa.edu.au/SAEDUIssuingCA_SHA2.cer"
-
-sudo /usr/bin/security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain saedu-int-ca.cer
+sudo /usr/bin/security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain /tmp/saedu-ca/saedu-int-ca.cer
 
 rm -rf /tmp/saedu-ca
 
