215afefacff0c4cf1b4f7be27981178f4a4496b9
Joe S <17176434+joeselway@users.noreply.github.com>
2019-05-29 00:50:00 +0930

    Update saedu-ca-trust-pkg-postflight.sh

--- schools.sa.edu.au/saedu-ca-trust-pkg-postflight.sh
+++ schools.sa.edu.au/saedu-ca-trust-pkg-postflight.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-echo 'Installing SAEDU Issuing CA'
+## Installs and trusts root and SHA256 intermediate CA identities for schools.sa.edu.au. 
+## 
+
+echo 'Installing SAEDU CA Identities'
 
 mkdir /tmp/saedu-ca
 
+curl -k -o "/tmp/saedu-ca/saedu-ca.cer" "http://ca.schools.sa.edu.au/saedurootca.cer"
 curl -k -o "/tmp/saedu-ca/saedu-int-ca.cer" "http://ca.schools.sa.edu.au/SAEDUIssuingCA_SHA2.cer"
 
+sudo /usr/bin/security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain /tmp/saedu-ca/saedu-ca.cer
 sudo /usr/bin/security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain /tmp/saedu-ca/saedu-int-ca.cer
 
 rm -rf /tmp/saedu-ca
